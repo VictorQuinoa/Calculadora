f7ec0f5c740042f104239b6591b9c213427766a3
VictorQuinoa <vquinoamunoz@danielcastelao.org>
2024-02-16 09:43:10 +0100

    Anotaciones

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,9 @@ public class Main {
 
     public static void main(String[] args) {
     Calculo obx = new Calculo();
+    //Peticion de numeros
      Float ret = obx.calculo(Float.parseFloat(JOptionPane.showInputDialog("Introduce el primer numero:")),Float.parseFloat(JOptionPane.showInputDialog("Introduce el segundo numero:")));
+    //Comprobacion de si se han introducido datos validos o de si ha fallado la aplicación con mensaje.
      if(ret == null){
          JOptionPane.showMessageDialog(null,"Algo ha fallado");
      }else{
