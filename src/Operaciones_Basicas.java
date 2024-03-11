868d34954f4b618e552fafcafc3a19924e110ef7
VictorQuinoa <vquinoamunoz@danielcastelao.org>
2024-03-11 09:58:33 +0100

    Commit

---
+++ src/Operaciones_Basicas.java
@@ -0,0 +1,91 @@
+import com.inout.Entrada;
+
+import javax.swing.*;
+
+public class Operaciones_Basicas {
+    /**
+     * Uso para el factory
+     */
+    private float num;
+    private float num2;
+    public static final int SUMA=1;
+    public static final int RESTA=2;
+    public static final int MULTIPLICACION=3;
+    public static final int DIVISION=4;
+    public static final int RAIZ=5;
+
+    /**
+     * Declaracion de los elementos a calcular
+     * @param num
+     * @param num2
+     */
+
+   public Operaciones_Basicas(float num, float num2) {
+
+       this.num = num;
+        this.num2= num2;
+    }
+
+
+    /**
+     * Metodo que contiene las operaciones
+     * @param a
+     * @param num
+     * @param num2
+     * @return
+     */
+    public static Float operaciones(int a,float num,float num2){
+
+   num= Float.parseFloat(JOptionPane.showInputDialog("Introduce el primer numero"));
+        num2= Float.parseFloat(JOptionPane.showInputDialog("Introduce el segundo numero"));
+
+        float resultado=0;
+        switch (a){
+            /**
+             * Suma
+             */
+            case SUMA:
+                resultado=num+num2;
+                break;
+            /**
+             * Resta
+             */
+            case RESTA:
+                resultado=num-num2;
+                break;
+            /**
+             * Multiplicación
+             */
+            case MULTIPLICACION:
+                resultado=num*num2;
+                break;
+            /**
+             * Division
+             */
+            case DIVISION:
+                resultado=num/num2;
+
+                break;
+            /**
+             * Calculo de Raiz seleccionando cuadrada o cúbica
+             */
+            case RAIZ:
+                int opc = Integer.parseInt(JOptionPane.showInputDialog("Selecciona para hacer del primer número: \n 1-Raiz cuadrada \n 2-Raiz cúbica"));
+                if (opc == 1) {
+                resultado = (float) Math.sqrt(num);
+            }else if (opc == 2) {
+                    resultado = (float) Math.cbrt(num);
+                }
+
+            default:
+                System.out.println("Operacion no Valida");
+        }
+        return  resultado;
+    }
+}
+
+
+
+
+
+
---
+++ src/readme.md
@@ -0,0 +1,15 @@
+Examen librerias:
+
+1-Abrimos el proyecto y le añadimos la libreria externa inout mediante la estructura de proyecto.
+
+2-Creo la rama raiz con git branch
+
+3-Añado el calculo de la raiz con seleccion para cuadrada o cúbica
+
+4-Añado raiz al factory y los declaro en la clase main para imprimir todos los resultados.
+
+5-Añado la opcion de introducir diferentes numeros para cada operacion con JOptionPane.
+
+6-Creo el jar dejando la main vacia para que pueda usarse como biblioteca
+
+7-Hago el programa repetitivo con do while
