2f7d14e9f75737f27ac0ed7376277d5167698413
VictorQuinoa <vquinoamunoz@danielcastelao.org>
2024-03-11 09:53:27 +0100

    Repetitivo

--- src/Main.java
+++ src/Main.java
@@ -5,56 +5,66 @@ public class Main {
      * Declarado para poder llamar al factory
      */
     static Float Salida;
-    public static void main(String[] args){
-// Salida.salidaDos("",Salida.VENTANA);
 
+    public static void main(String[] args) {
+// Salida.salidaDos("",Salida.VENTANA);
+        /**
+         * Estructura do while para hacer el programa repetitivo a peticion si se  introduce 0
+         *
+         */
+        int a =0;
+        do {
 
 /**
  * Impresion de la suma con opcion de fallo
  */
 
-        Salida= Operaciones_Basicas.operaciones(Operaciones_Basicas.SUMA,0,0);
-        if(Salida == null)
-            System.out.println("Algo fallo");
-        else
-            JOptionPane.showMessageDialog(null,"Suma: "+Salida);
+            Salida = Operaciones_Basicas.operaciones(Operaciones_Basicas.SUMA, 0, 0);
+            if (Salida == null)
+                System.out.println("Algo fallo");
+            else
+                JOptionPane.showMessageDialog(null, "Suma: " + Salida);
 /**
  * Impresion de la resta con opcion de fallo
  */
 
-        Salida = Operaciones_Basicas.operaciones(Operaciones_Basicas.RESTA,0,0);
-        if(Salida == null)
-            System.out.println("Algo fallo");
-        else
-            JOptionPane.showMessageDialog(null,"Resta: "+Salida);
-        /**
-         * Impresion de la multiplicacion con opcion de fallo
-         */
-
-        Salida = Operaciones_Basicas.operaciones(Operaciones_Basicas.MULTIPLICACION,0,0);
-        if(Salida == null)
-            System.out.println("Algo fallo");
-        else
-            JOptionPane.showMessageDialog(null,"Multiplicacion: "+Salida);
-        /**
-         * Impresion de la division con opcion de fallo
-         */
-
-        Salida = Operaciones_Basicas.operaciones(Operaciones_Basicas.DIVISION,0,0);
-        if(Salida == null)
-            System.out.println("Algo fallo");
-        else
-            JOptionPane.showMessageDialog(null,"Division: "+Salida);
-        /**
-         * Impresion de la raiz con opcion de fallo
-         */
-
-        Salida = Operaciones_Basicas.operaciones(Operaciones_Basicas.RAIZ,0,0);
-
-            if(Salida == null)
+            Salida = Operaciones_Basicas.operaciones(Operaciones_Basicas.RESTA, 0, 0);
+            if (Salida == null)
                 System.out.println("Algo fallo");
             else
-        JOptionPane.showMessageDialog(null,"Raiz: "+Salida);
-        }
+                JOptionPane.showMessageDialog(null, "Resta: " + Salida);
+            /**
+             * Impresion de la multiplicacion con opcion de fallo
+             */
+
+            Salida = Operaciones_Basicas.operaciones(Operaciones_Basicas.MULTIPLICACION, 0, 0);
+            if (Salida == null)
+                System.out.println("Algo fallo");
+            else
+                JOptionPane.showMessageDialog(null, "Multiplicacion: " + Salida);
+            /**
+             * Impresion de la division con opcion de fallo
+             */
+
+            Salida = Operaciones_Basicas.operaciones(Operaciones_Basicas.DIVISION, 0, 0);
+            if (Salida == null)
+                System.out.println("Algo fallo");
+            else
+                JOptionPane.showMessageDialog(null, "Division: " + Salida);
+            /**
+             * Impresion de la raiz con opcion de fallo
+             */
+
+            Salida = Operaciones_Basicas.operaciones(Operaciones_Basicas.RAIZ, 0, 0);
+
+            if (Salida == null)
+                System.out.println("Algo fallo");
+            else
+                JOptionPane.showMessageDialog(null, "Raiz: " + Salida);
+
+
+                  a= Integer.parseInt(JOptionPane.showInputDialog("Quieres continuar? \n0-si \n1-no"));
+        } while (a == 0);
     }
+}
 
