a94ced9ffbe06a02147fcde95e8e05e6efe12fa8
VictorQuinoa <vquinoamunoz@danielcastelao.org>
2024-03-11 09:57:56 +0100

    Commit

--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,71 @@
 import javax.swing.*;
 
 public class Main {
+    /**
+     * Declarado para poder llamar al factory
+     */
+    static Float Salida;
 
     public static void main(String[] args) {
-        int a = 0;
+// Salida.salidaDos("",Salida.VENTANA);
+        /**
+         * Estructura do while para hacer el programa repetitivo a peticion si se  introduce 0
+         *
+         */
+        int a =0;
         do {
-            Calculo obx = new Calculo();
-            //Peticion de numeros
-            Float ret = obx.calculo(Float.parseFloat(JOptionPane.showInputDialog("Introduce el primer numero:")), Float.parseFloat(JOptionPane.showInputDialog("Introduce el segundo numero:")));
-            //Comprobacion de si se han introducido datos validos o de si ha fallado la aplicación con mensaje.
-            if (ret == null) {
-                JOptionPane.showMessageDialog(null, "Algo ha fallado");
-            } else {
-                JOptionPane.showMessageDialog(null, "Operación finalizada");
-            }
-            a= Integer.parseInt(JOptionPane.showInputDialog("Quieres hacer otro calculo?\n 0-Si \n 1-No"));
-        }while(a==0);
+            JOptionPane.showMessageDialog(null,"El programa te pedira numeros para suma,resta,multiplicacion ,division y raiz en orden");
+
+/**
+ * Impresion de la suma con opcion de fallo
+ */
+
+            Salida = Operaciones_Basicas.operaciones(Operaciones_Basicas.SUMA, 0, 0);
+            if (Salida == null)
+                System.out.println("Algo fallo");
+            else
+                JOptionPane.showMessageDialog(null, "Suma: " + Salida);
+/**
+ * Impresion de la resta con opcion de fallo
+ */
+
+            Salida = Operaciones_Basicas.operaciones(Operaciones_Basicas.RESTA, 0, 0);
+            if (Salida == null)
+                System.out.println("Algo fallo");
+            else
+                JOptionPane.showMessageDialog(null, "Resta: " + Salida);
+            /**
+             * Impresion de la multiplicacion con opcion de fallo
+             */
+
+            Salida = Operaciones_Basicas.operaciones(Operaciones_Basicas.MULTIPLICACION, 0, 0);
+            if (Salida == null)
+                System.out.println("Algo fallo");
+            else
+                JOptionPane.showMessageDialog(null, "Multiplicacion: " + Salida);
+            /**
+             * Impresion de la division con opcion de fallo
+             */
+
+            Salida = Operaciones_Basicas.operaciones(Operaciones_Basicas.DIVISION, 0, 0);
+            if (Salida == null)
+                System.out.println("Algo fallo");
+            else
+                JOptionPane.showMessageDialog(null, "Division: " + Salida);
+            /**
+             * Impresion de la raiz con opcion de fallo
+             */
+
+            Salida = Operaciones_Basicas.operaciones(Operaciones_Basicas.RAIZ, 0, 0);
+
+            if (Salida == null)
+                System.out.println("Algo fallo");
+            else
+                JOptionPane.showMessageDialog(null, "Raiz: " + Salida);
+
+
+                  a= Integer.parseInt(JOptionPane.showInputDialog("Quieres continuar? \n0-si \n1-no"));
+        } while (a == 0);
     }
 }
+
