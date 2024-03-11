087acd66eb5cea310e0151d134d3a7b5c42ef0b9
VictorQuinoa <vquinoamunoz@danielcastelao.org>
2024-03-11 09:07:06 +0100

    Añadido calculo raiz y librerias.

--- src/Main.java
+++ src/Main.java
@@ -2,19 +2,14 @@ import javax.swing.*;
 
 public class Main {
 
-    public static void main(String[] args) {
-        int a = 0;
-        do {
-            Calculo obx = new Calculo();
-            //Peticion de numeros
-            Float ret = obx.calculo(Float.parseFloat(JOptionPane.showInputDialog("Introduce el primer numero:")), Float.parseFloat(JOptionPane.showInputDialog("Introduce el segundo numero:")));
-            //Comprobacion de si se han introducido datos validos o de si ha fallado la aplicación con mensaje.
-            if (ret == null) {
-                JOptionPane.showMessageDialog(null, "Algo ha fallado");
-            } else {
-                JOptionPane.showMessageDialog(null, "Operación finalizada");
-            }
-            a= Integer.parseInt(JOptionPane.showInputDialog("Quieres hacer otro calculo?\n 0-Si \n 1-No"));
-        }while(a==0);
+    public static void main(String[] args){
+// Salida.salidaDos("GOZ CABRON",Salida.VENTANA);
+
+            Float retorno = Operaciones_Basicas.operaciones(Operaciones_Basicas.SUMA,5,5);
+            if(retorno == null)
+                System.out.println("Algo fallo");
+            else
+                System.out.println("El resultado es: "+retorno);
+        }
     }
-}
+
