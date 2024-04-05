4fd4a7b8dd86db578af27e0bc3c3af60dc4e60ff
VictorQuinoa <vquinoamunoz@danielcastelao.org>
2024-04-05 10:04:25 +0200

    Tests positivos

---
+++ Test/TestCalculadora.java
@@ -0,0 +1,29 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class TestCalculadora {
+    @Test
+    public void testComprueboSumaPositivos (){
+        Assertions.assertEquals(1,Operaciones_Basicas.SUMA);
+    }
+    @Test
+    public void testComprueboRestaPositivos(){
+        Assertions.assertEquals(2,Operaciones_Basicas.RESTA);
+
+
+    }
+    @Test
+    public void testComprueboDivisionPositivos(){
+        Assertions.assertEquals(3,Operaciones_Basicas.DIVISION);
+    }
+@Test
+    public void testComprueboMultiplicacionPositivos(){
+        Assertions.assertEquals(3,Operaciones_Basicas.MULTIPLICACION);
+    }
+@Test
+    public void testComprueboRaizPositivos(){
+        Assertions.assertEquals(3,Operaciones_Basicas.DIVISION);
+    }
+
+
+}
